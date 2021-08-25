c356a6f700b5d18b833ccc32c60dd022e6b86d31
LucasKahl <lucaskahl63@googlemail.com>
2021-08-25 12:00:40 +0200

    erste Tests aufgesetzt 2021_08_25-12:== Lucas Kahl

---
+++ app/src/test/java/de/medieninformatik/rezepteapp02/RecyclerViewAdapterTest.java
@@ -0,0 +1,20 @@
+package de.medieninformatik.rezepteapp02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecyclerViewAdapterTest {
+
+
+
+    @org.junit.jupiter.api.Test
+    void onCreateViewHolder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onBindViewHolder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItemCount() {
+    }
+}
